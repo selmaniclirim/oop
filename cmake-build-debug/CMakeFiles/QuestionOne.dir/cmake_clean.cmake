83ad44b60aa231bd30556c2c49a350a39f056083
selmaniclirim <65732996+selmaniclirim@users.noreply.github.com>
2021-03-10 14:21:41 +0100

    Added Clion support

---
+++ cmake-build-debug/CMakeFiles/QuestionOne.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuestionOne.dir/LabOne/QuestionOne.c.obj"
+  "QuestionOne.exe"
+  "QuestionOne.exe.manifest"
+  "QuestionOne.pdb"
+  "libQuestionOne.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/QuestionOne.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/QuestionThree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuestionThree.dir/LabOne/QuestionThree.c.obj"
+  "QuestionThree.exe"
+  "QuestionThree.exe.manifest"
+  "QuestionThree.pdb"
+  "libQuestionThree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/QuestionThree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
